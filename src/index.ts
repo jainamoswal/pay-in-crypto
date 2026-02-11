47eb5703ea2530530bd5339b109a4ec909ae5e6f
Jainam Oswal <76057348+jainamoswal@users.noreply.github.com>
2026-02-11 20:15:19 +0530

    change emojis and added tron network

--- src/index.ts
+++ src/index.ts
@@ -6,87 +6,103 @@ const app = new Hono<{ Bindings: Env }>();
 export const cryptoNetworks: CryptoNetworkMap = {
   btc: {
     title: "Bitcoin (BTC)",
-    emoji: 5280862672131204613n,
+    emoji: 5409133571233319295n,
     coins: {
       usdt: {
         title: "BTC",
         address: "bc1qfzkh5k9qwj0qpvtpsp457mxxz3sal9aqc22vdf",
-        emoji: 5280862672131204613n
+        emoji: 5409133571233319295n
       }
     }
   },
   ton: {
     title: "TON (TON)",
-    emoji: 5460720028288557729n,
+    emoji: 5406976471153545018n,
     coins: {
       TON: {
         title: "TON",
         address: "UQC2RnutehG1As9I0x8PQ4oJsxdVo5u4WezeVDCMcrvn-mcv",
-        emoji: 5460720028288557729n
+        emoji: 5406976471153545018n
       },
       usdt: {
         title: "USDT",
         address: "UQC2RnutehG1As9I0x8PQ4oJsxdVo5u4WezeVDCMcrvn-mcv",
-        emoji: 5280963835790894176n
+        emoji: 5406841020769936275n
       }
     }
   },
   eth: {
     title: "Ethereum (ETH)",
-    emoji: 5280895833573697242n,
+    emoji: 5406930321729948822n,
     coins: {
       usdc: {
         title: "ETH",
         address: "0x21AE051b6266a980b5D47c2Ad2DdEE1C738E30AB",
-        emoji: 5280895833573697242n
+        emoji: 5406930321729948822n
       },
       usdt: {
         title: "USDT",
         address: "0x21AE051b6266a980b5D47c2Ad2DdEE1C738E30AB",
-        emoji: 5280963835790894176n
+        emoji: 5406841020769936275n
       }
     }
   },
   bsc: {
     title: "Binance Smart Chain (BNB)",
-    emoji: 5280535348378609271n,
+    emoji: 5406671889252781489n,
     coins: {
       bnb: {
         title: "BNB",
         address: "0x21AE051b6266a980b5D47c2Ad2DdEE1C738E30AB",
-        emoji: 5280535348378609271n
+        emoji: 5406671889252781489n
       },
       usdt: {
         title: "USDT",
         address: "0x21AE051b6266a980b5D47c2Ad2DdEE1C738E30AB",
-        emoji: 5280963835790894176n
+        emoji: 5406841020769936275n
+      }
+    }
+  },
+  trx:{
+    title: "Tron (TRX)",
+    emoji: 5406978786140918829n,
+    coins: {
+      trx: {
+        title: "TRX",
+        address: "TQh2qLZp7HjvVw5sYy9mLh8b3K8ZsG9zj",
+        emoji: 5406978786140918829n
+      },
+      usdt: {
+        title: "USDT",
+        address: "TQh2qLZp7HjvVw5sYy9mLh8b3K8ZsG9zj",
+        emoji: 5406841020769936275n
       }
     }
   },
   sol: {
     title: "Solana (SOL)",
-    emoji: 5289516520131535412n,
+    emoji: 5407016676342401484n,
     coins: {
       sol: {
         title: "SOL",
         address: "A1ff8CJPjuWRpVT4eyYvDoZbTndnuG7m351PNgw4PNJR",
-        emoji: 5289516520131535412n
+        emoji: 5407016676342401484n
       },
       usdt: {
         title: "USDT",
         address: "A1ff8CJPjuWRpVT4eyYvDoZbTndnuG7m351PNgw4PNJR",
-        emoji: 5280963835790894176n
+        emoji: 5406841020769936275n
       }
     }
   },
   ltc: {
     title: "Litecoin (LTC)",
-    emoji: 5370766044262570957n,
+    emoji: 5407128573125366746n,
     coins: {
       usdt: {
         title: "LTC",
         address: "ltc1qlld0jjeexj2dv76asmxmpr3cgmaulxr3wyar5h",
-        emoji: 5370766044262570957n
+        emoji: 5407128573125366746n
       }
     }
   }
