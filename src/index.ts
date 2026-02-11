a2835331405692bb9665c7e36f75b470cda6ea02
Jainam Oswal <76057348+jainamoswal@users.noreply.github.com>
2026-02-11 20:34:01 +0530

    update sequence

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,6 @@ export const cryptoNetworks: CryptoNetworkMap = {
     title: "Tron (TRX)",
     emoji: 5406978786140918829n,
     coins: {
-      trx: {
-        title: "TRX",
-        address: "TLoMytWUQbQbqELcffJPj1tfN8Rtca2ydC",
-        emoji: 5406978786140918829n
-      },
       usdt: {
         title: "USDT",
         address: "TLoMytWUQbQbqELcffJPj1tfN8Rtca2ydC",
@@ -38,18 +33,18 @@ export const cryptoNetworks: CryptoNetworkMap = {
         title: "USDC",
         address: "TLoMytWUQbQbqELcffJPj1tfN8Rtca2ydC",
         emoji: 5406575600380974539n
-      }
+      },
+      trx: {
+        title: "TRX",
+        address: "TLoMytWUQbQbqELcffJPj1tfN8Rtca2ydC",
+        emoji: 5406978786140918829n
+      },
     }
   },
   sol: {
     title: "Solana (SOL)",
     emoji: 5407016676342401484n,
     coins: {
-      sol: {
-        title: "SOL",
-        address: "A8ZNf3641MCzFTHN5h3Y4giWUbdCcRMqdu73EA5SHGEa",
-        emoji: 5407016676342401484n
-      },
       usdt: {
         title: "USDT",
         address: "A8ZNf3641MCzFTHN5h3Y4giWUbdCcRMqdu73EA5SHGEa",
@@ -59,7 +54,12 @@ export const cryptoNetworks: CryptoNetworkMap = {
         title: "USDC",
         address: "A8ZNf3641MCzFTHN5h3Y4giWUbdCcRMqdu73EA5SHGEa",
         emoji: 5406575600380974539n
-      }
+      },
+      sol: {
+        title: "SOL",
+        address: "A8ZNf3641MCzFTHN5h3Y4giWUbdCcRMqdu73EA5SHGEa",
+        emoji: 5407016676342401484n
+      },
     }
   },
   btc: {
@@ -88,11 +88,6 @@ export const cryptoNetworks: CryptoNetworkMap = {
     title: "Ethereum (ETH)",
     emoji: 5406930321729948822n,
     coins: {
-      eth: {
-        title: "ETH",
-        address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
-        emoji: 5406930321729948822n
-      },
       usdt: {
         title: "USDT",
         address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
@@ -102,18 +97,18 @@ export const cryptoNetworks: CryptoNetworkMap = {
         title: "USDC",
         address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
         emoji: 5406575600380974539n
-      }
+      },
+      eth: {
+        title: "ETH",
+        address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
+        emoji: 5406930321729948822n
+      },
     }
   },
   bsc: {
     title: "Binance Smart Chain (BNB)",
     emoji: 5406671889252781489n,
     coins: {
-      bnb: {
-        title: "BNB",
-        address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
-        emoji: 5406671889252781489n
-      },
       usdt: {
         title: "USDT",
         address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
@@ -123,7 +118,12 @@ export const cryptoNetworks: CryptoNetworkMap = {
         title: "USDC",
         address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
         emoji: 5406575600380974539n
-      }
+      },
+      bnb: {
+        title: "BNB",
+        address: "0x000cf1Cd76815f0bc3A87Ea9806bC7b259676176",
+        emoji: 5406671889252781489n
+      },
     }
   },
 };
